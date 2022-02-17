ce95dc2074427ac73e9c7c95a4e0aac65c3fa07d
Anubhav Apurva <anubhavh3@gmail.com>
2022-02-17 19:00:16 +0530

    Added notification service

---
+++ notification-service/src/main/java/com/example/notificationservice/controller/notifyController.java
@@ -0,0 +1,37 @@
+package com.example.notificationservice.controller;
+
+import com.example.notificationservice.entity.User;
+import com.example.notificationservice.service.sesEmailVerificationService;
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.cache.TemplateLoader;
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController(value = "/")
+public class notifyController {
+
+    private final sesEmailVerificationService verifyEmail;
+
+    @GetMapping("verify")
+    public ResponseEntity verifyEmail(@RequestParam("email") String emailId){
+        verifyEmail.verifyEmail(emailId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("email")
+    public ResponseEntity sendEmail(@RequestBody User user) throws TemplateException, MessagingException, IOException {
+        verifyEmail.sendEmail(user);
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ notification-service/src/main/java/com/example/notificationservice/service/sesEmailVerificationService.java
@@ -0,0 +1,87 @@
+package com.example.notificationservice.service;
+
+import com.example.notificationservice.entity.User;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ses.SesClient;
+
+import javax.annotation.PostConstruct;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+@RequiredArgsConstructor
+@Component
+public class sesEmailVerificationService {
+
+    private SesClient sesClient;
+    private final FreeMarkerConfigurer configurer;
+    private String fromEmail = "anubhavh3@gmail.com";
+    private String accessKey;
+    private String secretKey;
+
+    @PostConstruct
+    public void init(){
+        accessKey="AKIAQZIJ7MWRTEJGYUMV";
+        secretKey="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";
+
+        StaticCredentialsProvider staticCredentialsProvider = StaticCredentialsProvider
+                .create(AwsBasicCredentials.create(accessKey,secretKey));
+
+        sesClient = SesClient.builder()
+                .credentialsProvider(staticCredentialsProvider)
+                .region(Region.US_EAST_1)
+                .build();
+    }
+
+    public void verifyEmail(String emailId){
+        sesClient.verifyEmailAddress(req->req.emailAddress(emailId));
+    }
+
+    public void sendEmail(User user) throws IOException, TemplateException, MessagingException {
+        Map<String,Object> templateModel = new HashMap<>();
+        templateModel.put("user",user);
+        Template freeMarkerTemplate = configurer.getConfiguration().getTemplate("userwelcome.ftl");
+        String htmlBody = FreeMarkerTemplateUtils.processTemplateIntoString(freeMarkerTemplate,templateModel);
+        sendSimpleMessage(user.getEmailId(),"Welcome Email",htmlBody);
+    }
+
+    private void sendSimpleMessage(String toEmail, String subject, String body) throws MessagingException {
+        Properties props = System.getProperties();
+        props.put("mail.transport.protocol","smtp");
+        props.put("mail.smtp.port",587);
+        props.put("mail.smtp.starttls.enable","true");
+        props.put("mail.smtp.auth","true");
+        javax.mail.Session session = javax.mail.Session.getDefaultInstance(props);
+        MimeMessage msg = new MimeMessage(session);
+        msg.setFrom(fromEmail);
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+        msg.setSubject(subject);
+        msg.setContent(body,"text/html");
+        Transport transport = session.getTransport();
+        try {
+            String accessKey = "AKIAQZIJ7MWR6TGENQMH";
+            String secretKey = "BIui+1/WvKu/FaCbTRNNmn2DyDy9m/YsrbYgMuVXy4hM";
+            transport.connect("email-smtp.us-east-1.amazonaws.com", accessKey, secretKey);
+            transport.sendMessage(msg, msg.getAllRecipients());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            transport.close();
+        }
+    }
+
+}
