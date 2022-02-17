2edf08309b2261354693dbde2b7bcbf1dffcd998
Anubhav Apurva <anubhavh3@gmail.com>
2022-02-17 13:32:40 +0530

    secondary gateway

--- appointment-gateway/src/main/java/com/example/appointmentgateway/AppointmentGatewayApplication.java
+++ appointment-gateway/src/main/java/com/example/appointmentgateway/AppointmentGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.bmcgateway;
+package com.example.appointmentgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
-public class BmcGatewayApplication {
+public class AppointmentGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BmcGatewayApplication.class, args);
+		SpringApplication.run(AppointmentGatewayApplication.class, args);
 	}
 
 }
